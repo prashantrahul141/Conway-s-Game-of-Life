59a0d6c39002cdb72666bb4cca831b75f6cd27cc
Prashant <prashantrahul141@protonmail.com>
2023-07-29 07:06:07 +0530

    add: createCell, createAllCells functions.

--- out/index.js
+++ out/index.js
@@ -1,17 +1,36 @@
 "use strict";
-// each cell's dimension
+// each cell's dimension.
 const CELL_DIMENSION = 40;
 let COLUMNS = Math.floor(window.innerWidth / CELL_DIMENSION);
 let ROWS = Math.floor(window.innerHeight / CELL_DIMENSION);
+// parent wrapper element.
 const wrapper = document.getElementById('wrapper');
-wrapper.style.setProperty('--columns', COLUMNS.toString());
-wrapper.style.setProperty('--rows', ROWS.toString());
-const createAllCells = (num) => { };
-// incase of window resize.
+const handleClick = (index) => { };
+// creates indiviual cell.
+const createCell = (index) => {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.onclick = () => handleClick(index);
+    return cell;
+};
+// creates all cell with given quantity.
+const createAllCells = (quantity) => {
+    Array.from(Array(quantity)).map((c, index) => {
+        wrapper.appendChild(createCell(index));
+    });
+    wrapper.style.setProperty('--columns', COLUMNS.toString());
+    wrapper.style.setProperty('--rows', ROWS.toString());
+};
+// create initial cells.
+createAllCells(COLUMNS * ROWS);
+// creates intire grid incase of windows resizing.
 const createGrid = () => {
     wrapper.innerHTML = '';
     COLUMNS = Math.floor(window.innerWidth / CELL_DIMENSION);
     ROWS = Math.floor(window.innerHeight / CELL_DIMENSION);
-    // adding cells
+    wrapper.style.setProperty('--columns', COLUMNS.toString());
+    wrapper.style.setProperty('--rows', ROWS.toString());
+    createAllCells(COLUMNS * ROWS);
 };
+// on window resize, recreate all cells.
 window.onresize = createGrid;

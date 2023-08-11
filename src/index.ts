1fd231b6f565b91262f72a3c46c5f3e3201cbdad
Prashant <prashantrahul141@protonmail.com>
2023-08-11 07:23:41 +0530

    update cells when clicked on them, next step function, cache entire board's state

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,57 @@ const CELL_DIMENSION = 40;
 
 let COLUMNS = Math.floor(window.innerWidth / CELL_DIMENSION);
 let ROWS = Math.floor(window.innerHeight / CELL_DIMENSION);
+let PLAYING = false;
+let CACHED_DATA_STATE: Array<Array<number>> = [];
+
+const prepopulate_cache = () => {
+  CACHED_DATA_STATE = [];
+  for (let i = 0; i < ROWS; i++) {
+    CACHED_DATA_STATE.push(new Array(COLUMNS));
+  }
+
+  cacheCells();
+};
 
 // parent wrapper element.
 const wrapper = document.getElementById('wrapper') as HTMLDivElement;
+wrapper.innerText = '';
 
-const handleClick = (event: MouseEvent, index: number) => {
+// play button element.
+const play_button = document.getElementById('play-button') as HTMLButtonElement;
+
+// handleclick for cells.
+const handleClickPerCell = (event: MouseEvent, index: number) => {
   const thisCell = event.target as HTMLDivElement;
   const cellState = thisCell.getAttribute('data-state') || 'false';
+  const cellIndex = thisCell.getAttribute('data-index') as unknown as number;
 
   if (cellState === 'false') {
     thisCell.setAttribute('data-state', 'true');
     thisCell.classList.add('active-cell');
+    CACHED_DATA_STATE[(cellIndex - (cellIndex % COLUMNS)) / COLUMNS][
+      cellIndex % COLUMNS
+    ] = 1;
   } else {
     thisCell.setAttribute('data-state', 'false');
     thisCell.classList.remove('active-cell');
+    CACHED_DATA_STATE[(cellIndex - (cellIndex % COLUMNS)) / COLUMNS][
+      cellIndex % COLUMNS
+    ] = 0;
+  }
+};
+
+// cache all cells state.
+const cacheCells = () => {
+  const allCells = Array.from(wrapper.childNodes);
+
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLUMNS; j++) {
+      const _currentCell = allCells[i * j] as HTMLDivElement;
+
+      CACHED_DATA_STATE[i][j] =
+        _currentCell.getAttribute('data-state') === 'false' ? 0 : 1;
+    }
   }
 };
 
@@ -24,9 +61,32 @@ const handleClick = (event: MouseEvent, index: number) => {
 const createCell = (index: number) => {
   const cell = document.createElement('div');
   cell.setAttribute('data-state', 'false');
+  cell.setAttribute('data-index', index.toString());
   cell.classList.add('cell');
-  cell.onclick = (event) => handleClick(event, index);
+  cell.onclick = (event) => handleClickPerCell(event, index);
   return cell;
+};
+
+// helper function to set playing variable and update states.
+const setPlaying = (target: boolean) => {
+  PLAYING = target;
+  const play_svg_pause = document.getElementById(
+    'play-button-pause'
+  ) as HTMLElement;
+  const play_svg_play = document.getElementById(
+    'play-button-play'
+  ) as HTMLElement;
+
+  // changing button svg.
+  if (PLAYING) {
+    play_svg_play.classList.add('hidden');
+    play_svg_pause.classList.remove('hidden');
+
+    nextStep();
+  } else {
+    play_svg_play.classList.remove('hidden');
+    play_svg_pause.classList.add('hidden');
+  }
 };
 
 // creates all cell with given quantity.
@@ -36,6 +96,8 @@ const createAllCells = (quantity: number) => {
   });
   wrapper.style.setProperty('--columns', COLUMNS.toString());
   wrapper.style.setProperty('--rows', ROWS.toString());
+  setPlaying(false);
+  prepopulate_cache();
 };
 
 // create initial cells.
@@ -56,3 +118,18 @@ const createGrid = () => {
 
 // on window resize, recreate all cells.
 window.onresize = createGrid;
+
+// move forward the simulation.
+const nextStep = () => {
+  console.info('Moving next step.');
+  if (PLAYING) {
+    setTimeout(nextStep, 0.5);
+  }
+};
+// handleCell for play button.
+const handleClickPlayButton = (event: MouseEvent) => {
+  setPlaying(!PLAYING);
+  // changing playing state variable.
+};
+
+play_button.onclick = handleClickPlayButton;
